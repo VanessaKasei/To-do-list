c37414ce3d21e007e1f7517c02c4e019a2509346
VanessaKasei <kaseivanessa@gmail.com>
2023-11-01 02:18:40 +0300

    Task input field

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Tasks = () => {
-    return(
-        <div className="to-dos">
-            <div className="title">
-                <h3>My to do's</h3>
-            </div>
+  const [taskValue, setTaskValue] = useState(""); //store the added task using a string
 
-        </div>
-    );
-}
+  let onChange = (event) => {
+    setTaskValue(event.target.value);
+  };
+
+  return (
+    <div className="to-dos">
+      <div className="title">
+        <h3>My to do's</h3>
+      </div>
+      <div>
+        <input
+          placeholder="Enter a task..."
+          value={taskValue}
+          onChange={onChange}
+        />
+
+      </div>
+    </div>
+  );
+};
 
 export default Tasks;
