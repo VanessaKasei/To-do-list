ca09ae1409f7abd9d7226dbd7cdb5ba36613e69e
VanessaKasei <kaseivanessa@gmail.com>
2023-11-01 02:08:49 +0300

    App title

---
+++ src/Components/Tasks.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Tasks = () => {
+    return(
+        <div className="to-dos">
+            <div className="title">
+                <h3>My to do's</h3>
+            </div>
+
+        </div>
+    );
+}
+
+export default Tasks;
