424483c542be62c45d5a0fa77012adb5dbf38c3b
VanessaKasei <kaseivanessa@gmail.com>
2023-11-01 02:24:51 +0300

    Add function for the add button

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 
-
 const Tasks = () => {
   const [taskValue, setTaskValue] = useState(""); //store the added task using a string
-
+  const [tasks, setTasks] = useState([]); //store all tasks in an array
   let onChange = (event) => {
     setTaskValue(event.target.value);
+  };
+
+  const Add = () => {
+    if (taskValue.trim() !== "") {
+      setTasks([...tasks, taskValue]); //add new task
+    }
+    setTaskValue(""); //clear the entered task
   };
 
   return (
