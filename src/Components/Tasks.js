d26ec6a4a63f9465eacf66950b585b943d0238e7
VanessaKasei <kaseivanessa@gmail.com>
2023-11-01 03:18:41 +0300

    edit a task that is already in the lis

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 const Tasks = () => {
   const [taskValue, setTaskValue] = useState(""); //store the added task using a string
   const [tasks, setTasks] = useState([]); //store all tasks in an array
+  const [editingTask, setEditingTask] = useState(null); // keep track of the task being edited
+  const [editedText, setEditedText] = useState(""); //update text of the task
   let onChange = (event) => {
     setTaskValue(event.target.value);
   };
@@ -17,6 +19,18 @@ const Tasks = () => {
   const deleteTask = (removedTask) => {
     setTasks(tasks.filter((task) => task !== removedTask));
   }; // delete a task
+  const editTask = (taskToEdit) => {
+    setEditingTask(taskToEdit);
+    setEditedText(taskToEdit);
+  };
+
+  const saveEditedTask = () => {
+    const updatedTasks = tasks.map((task) =>
+      task === editingTask ? editedText : task
+    );
+    setTasks(updatedTasks);
+    setEditingTask(null); // Exit edit mode
+  };
 
   return (
     <div className="to-dos">
@@ -37,8 +51,24 @@ const Tasks = () => {
         <ol>
           {tasks.map((task, index) => (
             <li key={index}>
-              {task}
-              <button onClick={() => deleteTask(task)}>Delete</button>
+              {editingTask === task ? ( // Checks if the task is being edited
+                <div>
+                  <input
+                    type="text"
+                    value={editedText}
+                    onChange={(event) => setEditedText(event.target.value)}
+                  />
+                  <button onClick={() => saveEditedTask(editedText, task)}>
+                    Save 
+                  </button>
+                </div>
+              ) : (
+                <div>
+                  {task}
+                  <button onClick={() => deleteTask(task)}>Delete</button>
+                  <button onClick={() => editTask(task)}>Edit</button>
+                </div>
+              )}
             </li> //create a list of added tasks
           ))}
         </ol>
